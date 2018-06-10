47352d44cfc36107e132f2bf5404d79a70c86755
Leoltron <leoole5@gmail.com>
2018-06-10 17:32:41 +0500

    Removed Java libs from infrastructure ScalarType class

--- PayDebt/ScalarType.cs
+++ PayDebt/ScalarType.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Java.Lang;
 
 namespace PayDebt
 {
@@ -20,11 +19,8 @@ namespace PayDebt
             (ScalarType<TValue, TUnit> first, ScalarType<TValue, TUnit> second)
         {
             if (Equals(first.Unit, second.Unit)) return;
-            throw new IllegalArgumentException(
+            throw new ArgumentException(
                 $"Unit of measurement of {first} ({first.Unit}) and {second} ({second.Unit}) are not equal");
         }
-
-        
-
     }
 }
