36057b5c82a14592b66daa9507b04784530e4c66
Leoltron <leoole5@gmail.com>
2018-06-10 16:55:28 +0500

    Changed ValueType to the one created on OOP practice

--- PayDebt/ValueType.cs
+++ PayDebt/ValueType.cs
@@ -1,59 +1,99 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
 
 namespace PayDebt
 {
-    public class ValueType<T>
+    public class ValueType<T> where T : class
     {
-        private static PropertyInfo[] properties = null;
-        private int hashCode;
-        private bool calculated = false;
+        private static readonly Func<T, int> GetHashCodeFast;
 
-        private PropertyInfo[] GetProperties()
+        static ValueType()
         {
-            return properties ??
-                   (properties = GetType()
-                       .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                       .OrderBy(p => p.Name).ToArray());
+            var props = GetPublicInstanceProperties();
+
+
+            var valueTypeParam = Expression.Parameter(typeof(T));
+            var ghc = typeof(object).GetMethod("GetHashCode");
+
+            if(ghc == null)
+                throw new NullReferenceException("Unexpected null from typeof(object).GetMethod(\"GetHashCode\")");
+
+            if (props.Length == 1)
+                GetHashCodeFast = Expression.Lambda<Func<T, int>>(
+                        Expression.Call(Expression.MakeMemberAccess(valueTypeParam, props[0]), ghc), valueTypeParam)
+                    .Compile();
+            else
+            {
+                var p = Expression.Add(
+                    Expression.Call(Expression.MakeMemberAccess(valueTypeParam, props[0]), ghc),
+                    Expression.Call(Expression.MakeMemberAccess(valueTypeParam, props[1]), ghc)
+                );
+                for (var i = 2; i < props.Length; i++)
+                    p = Expression.Add(p, Expression.Call(Expression.MakeMemberAccess(valueTypeParam, props[i]), ghc));
+                GetHashCodeFast = Expression.Lambda<Func<T, int>>(p, valueTypeParam).Compile();
+            }
         }
 
-        public bool Equals(T other)
+        private static PropertyInfo[] GetPublicInstanceProperties()
         {
-            if (other == null) return false;
-            if (other.GetType() != GetType()) return false;
-            return GetProperties()
-                .All(p => Equals(p.GetValue(this), p.GetValue(other)));
+            return typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
         }
 
-        public override bool Equals(object obj)
+        private static IEnumerable<PropertyInfo> GetVisibleInstancePropertiesOrdered()
         {
-            if (obj == null) return false;
-            if (obj.GetType() != GetType()) return false;
-            return (bool)obj
-                .GetType()
-                .GetMethod("Equals", new[] { obj.GetType() })
-                .Invoke(obj, new object[] { this });
+            return GetPublicInstanceProperties().Where(p => p.CanRead)
+                .OrderBy(p => p.Name, StringComparer.Ordinal);
         }
 
-        public override int GetHashCode()
+        private IEnumerable<string> ToStringAllVisibleInstanceProperties()
         {
-            if (calculated)
-                return hashCode;
-            calculated = true;
-            return hashCode = GetProperties()
-                .Select(p => p.GetValue(this).GetHashCode())
-                .Aggregate((x, y) => x * 397 ^ y);
+            return GetVisibleInstancePropertiesOrdered()
+                .Select(property => $"{property.Name}: {property.GetValue(this)}");
         }
 
         public override string ToString()
         {
-            var builder = new StringBuilder();
-            builder.Append(GetType().Name);
-            builder.Append('(');
-            builder.Append(string.Join("; ", GetProperties().Select(p => p.Name + ": " + p.GetValue(this))));
-            builder.Append(')');
-            return builder.ToString();
+            var propString = string.Join("; ", ToStringAllVisibleInstanceProperties());
+
+            return $"{typeof(T).Name}({propString})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof(T)) return false;
+            return Equals((T) obj);
+        }
+
+        public bool Equals(T other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+
+            return !(from property in GetVisibleInstancePropertiesOrdered()
+                let value = property.GetValue(this)
+                where !Equals(value, property.GetValue(other))
+                select value).Any();
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeFast != null && this is T ? GetHashCodeFast(this as T) : GetHashCodeSlow();
+        }
+
+        private int GetHashCodeSlow()
+        {
+            unchecked
+            {
+                return (from property in GetVisibleInstancePropertiesOrdered()
+                        select property.GetValue(this)
+                        into value
+                        select value?.GetHashCode() ?? 0)
+                    .Aggregate(0, (current, hash) => (current * 397) ^ hash);
+            }
         }
     }
 }
