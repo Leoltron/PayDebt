0e6852abefb12d95e18cc7b0b4b871984265c34a
Leoltron <leoole5@gmail.com>
2018-06-12 17:36:15 +0500

    Fixed InfrastructureTests project

--- InfrastructureTests/Entity_Should.cs
+++ InfrastructureTests/Entity_Should.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Infrastructure;
 using NUnit.Framework;
 
---
+++ InfrastructureTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InfrastructureTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InfrastructureTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5dee45e3-4291-4127-bde1-f6b75ab686f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
