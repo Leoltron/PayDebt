b951005cd7163b4d4122068556e4b5054595835e
lowgear <lowgear1000@gmail.com>
2018-06-12 17:17:48 +0500

    Tests

---
+++ InfrastructureTests/Entity_Should.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using Infrastructure;
+using NUnit.Framework;
+
+namespace InfrastructureTests
+{
+    [TestFixture]
+    public class Entity_Should
+    {
+        [Test]
+        public void Equals_ShouldReturnFalseOnNull()
+        {
+            new TestEntity(0).Equals(null).Should().BeFalse();
+        }
+
+        [Test]
+        public void Equals_ShouldReturnTrueOnSelf()
+        {
+            var entity = new TestEntity(0);
+            entity.Equals(entity).Should().BeTrue();
+        }
+    }
+
+    public class TestEntity : Entity<int>
+    {
+        public TestEntity(int id) : base(id)
+        {
+        }
+    }
+}
