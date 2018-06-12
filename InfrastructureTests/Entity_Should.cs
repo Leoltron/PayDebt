32f8a25f51f055966458182e948ed007224fc01e
lowgear <lowgear1000@gmail.com>
2018-06-12 17:58:24 +0500

    Tests for Entity done.

--- InfrastructureTests/Entity_Should.cs
+++ InfrastructureTests/Entity_Should.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Infrastructure;
 using NUnit.Framework;
+// ReSharper disable InconsistentNaming
 
 namespace InfrastructureTests
 {
@@ -18,6 +19,58 @@ namespace InfrastructureTests
         {
             var entity = new TestEntity(0);
             entity.Equals(entity).Should().BeTrue();
+        }
+
+        [Test]
+        public void Equals_ShouldReturnTrueOnOtherOfSameId()
+        {
+            var entity = new TestEntity(0);
+            var other = new TestEntity(0);
+            entity.Equals(other).Should().BeTrue();
+        }
+
+        [Test]
+        public void Equals_ShouldReturnFalseOnOtherOfDifferentId()
+        {
+            var entity = new TestEntity(0);
+            var other = new TestEntity(1);
+            entity.Equals(other).Should().BeFalse();
+        }
+
+        [Test]
+        public void Equals_ShouldReturnFalseOnObjectsOfOtherTypes()
+        {
+            var entity = new TestEntity(0);
+            var other = new object();
+            entity.Equals(other).Should().BeFalse();
+        }
+    }
+
+    [TestFixture]
+    public class GetHashCode_Should
+    {
+        [Test]
+        public void GetHashCode_ShouldReturnSameForSameIds()
+        {
+            for (var id = 0; id < 10; id++)
+            {
+                var one = new TestEntity(id);
+                var other = new TestEntity(id);
+                one.GetHashCode().Should().Be(other.GetHashCode());   
+            }
+        }
+    }
+
+    [TestFixture]
+    public class ToString_Should
+    {
+        [Test]
+        public void ToString_ShouldReturnStringOfCertainFormat()
+        {
+            for (var id = 0; id < 10; id++)
+            {
+                new TestEntity(id).ToString().Should().Be($"TestEntity(Id: {id})");
+            }
         }
     }
 
