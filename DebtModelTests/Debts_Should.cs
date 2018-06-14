99fdab9ef913a79bd4dcad5c69acbb630c93c0d7
lowgear <lowgear1000@gmail.com>
2018-06-15 00:09:51 +0500

    Started Debts testing

---
+++ DebtModelTests/Debts_Should.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using DebtModel;
+using FakeItEasy;
+using Infrastructure;
+using NUnit.Framework;
+
+namespace DebtModelTests
+{
+    [TestFixture]
+    public class Debts_Should
+    {
+        public IEntityStorageAccess<int, Debt> StorageAccess { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            StorageAccess = A.Fake<IEntityStorageAccess<int, Debt>>();
+            A.CallTo(() => StorageAccess.LoadEntities())
+                .Returns(new[] {new Debt(1, new Contact("Alexander"), ), })
+        }
+
+        [Test]
+        public void GetNextId_ShouldReturnIntegersFromZero()
+        {
+            var debts = new Debts(new Dictionary<int, Debt>());
+        }
+    }
+}
